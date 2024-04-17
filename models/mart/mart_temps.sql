319eb0288980eac38705dfd63f79e4e416c7cae5
liam-clowes <clowesliam@gmail.com>
2024-04-17 17:40:11 +0200

    daily_push_4_17/04/24

--- models/mart/mart_temps.sql
+++ models/mart/mart_temps.sql
@@ -3,6 +3,7 @@ select date
         ,month_of_year
         ,month -- newly added numerical month
         ,city
+        ,country
         ,round(avg(max_temp_c),2) as max_temp_c
         ,round(avg(min_temp_c),2) as min_temp_c
         ,round(avg(avg_temp_c),2) as avg_temp_c
@@ -12,6 +13,7 @@ group by date
     ,month_of_year
     ,month --- group on
     ,city
+    ,country
 order by city asc -- optional, alphabetical order
     ,year asc
     ,month asc -- order by numerical month
