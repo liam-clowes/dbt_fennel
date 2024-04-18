524c88f56ccd733a75c964c8cfcc3cbb903e93b4
liam-clowes <clowesliam@gmail.com>
2024-04-18 15:01:27 +0200

    daily_push_dbt_2_18/04/24

--- models/mart/mart_temps_hour.sql
+++ models/mart/mart_temps_hour.sql
@@ -18,8 +18,23 @@ select date
         ,uv
 from {{ref('mart_forecast_hour')}}
 group by date
-    ,city
-    ,country
+        ,city
+        ,country
+        ,time_epoch
+        ,date_time
+        ,is_day
+        ,time
+        ,hour
+        ,month_of_year
+        ,day_of_week
+        ,condition_text
+        ,temp_c
+        ,wind_kph
+        ,precip_mm
+        ,feelslike_c
+        ,windchill_c
+        ,chance_of_rain
+        ,uv
 order by city asc -- optional, alphabetical order
 
             
